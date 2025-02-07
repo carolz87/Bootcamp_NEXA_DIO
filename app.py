6f26bcded832ebafd86ab991f0dbc4bc8bca36e7
Carolz87 <carolfranco.oliveira@gmail.com>
2025-02-07 14:25:46 -0300

    Commit inicial para projeto de teste base de AWS Rekogition

--- app.py
+++
@@ -1,8 +0,0 @@
-import boto3
-
-def lambda_handler(event, context):
-    textract = boto3.client('textract')
-    response = textract.detect_document_text(
-        Document={'S3Object': {'Bucket': event['bucket'], 'Name': event['name']}}
-    )
-    return response
