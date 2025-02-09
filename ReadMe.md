a9fbc6eee486824ba7e5deeb1a407e949d2695e8
Carolz87 <carolfranco.oliveira@gmail.com>
2025-02-09 15:46:15 -0300

    Inclusão do projeto em repositório

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,33 @@
-# AWS Rekognition 
+# AWS Rekognition
+
+Este projeto é um exercício do Bootcamp Nexa IA da DIO. O objetivo do código é utilizar o serviço AWS Rekognition para detectar celebridades em imagens e desenhar uma `bounding-box` em volta das celebridades detectadas.
+
+
+## Funcionalidade do Código
+
+O código em `main.py` realiza as seguintes etapas:
+
+1. **Carregar Imagens**: As imagens são carregadas a partir do diretório `Img/`.
+2. **Reconhecimento de Celebridades**: Utiliza o serviço AWS Rekognition para reconhecer celebridades nas imagens.
+3. **Desenhar Bounding-Box**: Desenha uma bounding-box em volta das celebridades detectadas e salva a imagem resultante.
+
+### Funções Principais
+
+- `caminho(arquivo: str) -> str`: Retorna o caminho completo de um arquivo no diretório `Img/`.
+- `reconhecimento(foto: str) -> RecognizeCelebritiesResponseTypeDef`: Realiza o reconhecimento de celebridades utilizando o AWS Rekognition.
+- `faz_caixa(image_path: str, output_path: str, face_details: list[CelebrityTypeDef])`: Desenha uma bounding-box em volta das celebridades detectadas e salva a imagem resultante.
+
+### Como Executar
+
+1. **Variáveis de Ambiente**: Edite o arquivo `.env.example` com suas respectivas chaves de acesso da AWS. Após salvar o arquivo altere o nome para `.env`
+
+2. **Dependências**: Certifique-se de ter as dependências instaladas:
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+3. **Executar o Script**: Execute o script `main.py`:
+    ```sh
+    python main.py
+    ```
+
---
+++ main.py
@@ -0,0 +1,79 @@
+from pathlib import Path
+import boto3
+from mypy_boto3_rekognition.type_defs import (CelebrityTypeDef,RecognizeCelebritiesResponseTypeDef)
+from PIL import Image, ImageDraw, ImageFont
+import os
+
+def conecta():
+    session = boto3.Session(
+        aws_access_key_id= os.getenv('ACESS_KEY'),
+        aws_secret_access_key= os.getenv('SECRET_ACCESS_KEY'),
+        region_name=os.getenv('REGION')
+    )
+
+    client = session.client("rekognition")
+    
+    return client
+
+def desconecta(client):
+    client.close()
+    
+
+def caminho(arquivo: str) -> str:
+    return str(Path(__file__).parent / "img" / arquivo)
+
+
+def reconhecimento(foto: str) -> RecognizeCelebritiesResponseTypeDef:
+    with open(foto, "rb") as image:
+        return client.reconhecimento(Image={"Bytes": image.read()})
+
+
+def faz_caixa(image_path: str, output_path: str, face_details: list[CelebrityTypeDef]):
+    image = Image.open(image_path)
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype("Ubuntu-R.ttf", 20)
+
+    width, height = image.size
+
+    for face in face_details:
+        box = face["Face"]["BoundingBox"]  
+        left = int(box["Left"] * width)  
+        top = int(box["Top"] * height)  
+        right = int((box["Left"] + box["Width"]) * width)  
+        bottom = int((box["Top"] + box["Height"]) * height)  
+
+        confidence = face.get("MatchConfidence", 0)
+        if confidence > 80:
+            draw.rectangle([left, top, right, bottom], outline="red", width=3)
+
+            text = face.get("Name", "")
+            position = (left, top - 20)
+            bbox = draw.textbbox(position, text, font=font)
+            draw.rectangle(bbox, fill="red")
+            draw.text(position, text, font=font, fill="white")
+
+    image.save(output_path)
+    print(f"Imagem salva com resultados em : {output_path}")
+
+
+if __name__ == "__main__":
+    
+    client = conecta()
+
+    caminho_foto = [
+        caminho("001.jpg"),
+        caminho("002.jpg"),
+        caminho("003.jpg"),
+       
+    ]
+
+    for endereco in caminho_foto:
+        response = reconhecimento(endereco)
+        faces = response["CelebrityFaces"]
+        if not faces:
+            print(f"Não foram encontrados famosos para a imagem: {endereco}")
+            continue
+        output_path = caminho(f"{Path(endereco).stem}-resultado.jpg")
+        faz_caixa(endereco, output_path, faces)
+    
+    desconecta(client)
